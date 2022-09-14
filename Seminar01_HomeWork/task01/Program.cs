c50bbee57848eb82ad25fd9e5caba88253c7f15c
xkostix <mkostik@yandex.ru>
2022-09-15 01:22:32 +0700

    sem01 add back program.cs

---
+++ Seminar01_HomeWork/task01/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();  // ?? "0" - don't work with empty string
+    if (strValue == "") strValue = "0";
+    int value = int.Parse(strValue);
+    return (value);
+}
+
+int value1 = Prompt("Введите число a: ");
+int value2 = Prompt("Введите число b: ");
+
+if (value1 == value2)
+{
+    System.Console.WriteLine("Числа равны");
+}
+else
+{
+    if (value1 > value2)
+    {
+        System.Console.WriteLine($"Число {value1} больше, чем {value2}");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {value2} больше, чем {value1}");
+    }
+}
