31d0031b0e36735e1d462636c26dbe7f28ee774c
xkostix <mkostik@yandex.ru>
2022-11-18 23:49:26 +0700

    ReWrite task04

--- Seminar01_HomeWork/task04/Program.cs
+++ Seminar01_HomeWork/task04/Program.cs
@@ -1,17 +1,9 @@
-﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-int Prompt(string message)
-{
-    Console.Write(message);
-    string strValue = Console.ReadLine() ?? "0";
-    if (strValue == "") strValue = "0";     // эта строка исключает ошибку
-    int value = int.Parse(strValue);
-    return (value);
-}
-
-int value1 = Prompt("Введите число: ");
+Console.Write("Введите число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
 if (value1 < 2)
 {
     System.Console.WriteLine("Число должно быть не меньше 2.");
