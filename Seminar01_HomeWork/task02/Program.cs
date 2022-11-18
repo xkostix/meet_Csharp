62ec36bf0729e1619b17e53439ec0cd255658f99
xkostix <mkostik@yandex.ru>
2022-11-18 23:29:06 +0700

    ReWrite task02

--- Seminar01_HomeWork/task02/Program.cs
+++ Seminar01_HomeWork/task02/Program.cs
@@ -1,20 +1,15 @@
-﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-int Prompt(string message)
-{
-    Console.Write(message);
-    string strValue = Console.ReadLine() ?? "0";  //Вернул ??, чтобы компилятор не ругался
-    if (strValue == "") strValue = "0";     // эта строка исключает ошибку
-    int value = int.Parse(strValue);
-    return (value);
-}
+Console.Write("Введите первое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int value3 = Convert.ToInt32(Console.ReadLine());
 
-int value1 = Prompt("Введите первое число: ");
-int value2 = Prompt("Введите второе число: ");
-int value3 = Prompt("Введите третье число: ");
 int max = value1;
 if (max < value2) max = value2;
 if (max < value3) max = value3;
