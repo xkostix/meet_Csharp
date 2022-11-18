11690bcf74bbb22a8205e9a41feb075bec143414
xkostix <mkostik@yandex.ru>
2022-11-18 23:34:58 +0700

    ReWrite task03

--- Seminar01_HomeWork/task03/Program.cs
+++ Seminar01_HomeWork/task03/Program.cs
@@ -1,23 +1,16 @@
-﻿//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-//4 -> да
-//-3 -> нет
-//7 -> нет
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
-int Prompt(string message)
-{
-    Console.Write(message);
-    string strValue = Console.ReadLine() ?? "0";
-    if (strValue == "") strValue = "0";     // эта строка исключает ошибку
-    int value = int.Parse(strValue);
-    return (value);
-}
+Console.Write("Введите число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
 
-int value1 = Prompt("Введите число: ");
 if ((value1 % 2) == 0)
 {
     System.Console.WriteLine($"{value1} - четное число");
 }
 else
 {
-    System.Console.WriteLine($"{value1} - нечетное число");
+    System.Console.WriteLine($"{value1} - НЕчетное число");
 }
