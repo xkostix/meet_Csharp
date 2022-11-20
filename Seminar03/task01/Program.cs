1ad9b474908616e1f338534058492fd50f755c3a
xkostix <mkostik@yandex.ru>
2022-11-20 18:35:25 +0700

    Seminar03

---
+++ Seminar03/task01/Program.cs
@@ -0,0 +1,28 @@
+﻿// X+ Y+ = 1   x+ y- = 4
+// -x -y = 3  -x +y = 2
+
+Console.Write("Введите Х ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Числа не должны быть равны нулю");
+}
+else if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+else
+{
+    Console.WriteLine("2 четверть");
+}
---
+++ Seminar03/task03/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+Console.WriteLine("Введите x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+Console.WriteLine(distance);
