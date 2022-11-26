39d63e6da8f6234b394e010761a5db5739c1a807
xkostix <mkostik@yandex.ru>
2022-11-27 02:07:24 +0700

    Homework04 task03

---
+++ Seminar04_HomeWork/task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] numbers = new int[8];
+
+Console.Write("[");  //Вывод открывающей скобки
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 100);
+    Console.Write(numbers[i]);
+
+    if (i < (numbers.Length - 1)) Console.Write(", ");
+    else Console.WriteLine("]");   //Закрывающая скобка
+}
