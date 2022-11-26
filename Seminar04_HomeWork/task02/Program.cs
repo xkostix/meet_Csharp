eca8d828bd000d504890f33dfe0d81a0042e543a
xkostix <mkostik@yandex.ru>
2022-11-27 01:49:28 +0700

    HomeWork04 task02

---
+++ Seminar04_HomeWork/task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int i = num % 10;
+while (num != 0)
+{
+    sum = sum + i;
+    num = num / 10;
+    i = num % 10;
+}
+
+Console.WriteLine(sum);
