09161ac53ae5d383cda80bcfa8d22fd268386c7b
xkostix <mkostik@yandex.ru>
2022-12-11 00:34:23 +0700

    HomeWork05 task01

---
+++ Seminar05_HomeWork/task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[5];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0) count++;
+}
+
+PrintArray(array);
+Console.WriteLine($"Четных чисел {count}");
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++) Console.Write($"{arr[i]} ");
+    Console.WriteLine("]");
+}
