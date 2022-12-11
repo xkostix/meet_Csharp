f5df72fea745a939befbfe7e748a1d412655330b
xkostix <mkostik@yandex.ru>
2022-12-11 20:28:47 +0700

    HomeWork06 Task01

---
+++ Seminar06_HomeWork/task01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через Enter, в конце введите пустую строку:");
+
+string str;
+int num;
+int count = 0;
+
+while (true)
+{
+    str = Console.ReadLine();
+    if (str.Trim() == "") break;
+    num = Convert.ToInt32(str);
+    if (num > 0) count++;
+}
+
+Console.WriteLine($"Чисел больше нуля: {count}");
